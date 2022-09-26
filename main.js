c4f8774c27e908604ba79dd88a5b9a487c7efcb1
SalumMax <salumaxx@gmail.com>
2022-09-26 15:52:38 +1000

    first commit

---
+++ main.js
@@ -0,0 +1,80 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.querySelector('#count');
+const total = document.querySelector('#total');
+const movieSelect = document.querySelector('#movie');
+const movieImage = document.querySelector('.movie-img');
+
+
+populateUI();
+let ticketPrice = +movieSelect.value; //+ at the start is to convert value to a number
+
+//Get data from local storage and populate UI
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex; //setting stored selected movie index back to movieSelect
+  }
+
+  const selectedMovieImage = localStorage.getItem('selectedMovieImage');
+}
+
+// save selected movie index and price to local storage
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+  
+}
+
+//update total and count
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  let seatsCount = selectedSeats.length;
+  count.innerText = seatsCount;
+  total.innerText = seatsCount * ticketPrice;
+}
+
+// less performant way would be using forEach on each seat and only show the clicked. Instead, I select the div which wraps all seats and  only show the elements (seats) clicked.
+container.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+    updateSelectedCount(); //once selections are made, update the function which calculates the number of seats and the total price of tickets.
+  }
+});
+
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value; // gets price (value) of the selected movie in number format
+  setMovieData(e.target.selectedIndex, e.target.value); //runs the function to store selected movie name (e.target.selectedIndex) and price (e.target.value) to local storage
+  let movieName = e.target.options[e.target.selectedIndex].text;
+  if (movieName.includes('Avatar')) {
+    movieImage.src = 'img/avatar.jpeg';
+  } else if (movieName.includes('Joker')) {
+    movieImage.src = 'img/joker.jpeg';
+  } else if (movieName.includes('Star Wars')) {
+    movieImage.src = 'img/starwars.jpeg';
+  } else if (movieName.includes('Avengers')) {
+    movieImage.src = 'img/avengers.jpeg';
+  }
+  updateSelectedCount();
+});
+
+//initial count and total set
+
+updateSelectedCount();
